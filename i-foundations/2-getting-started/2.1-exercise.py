2112f809a3abaab43fe02b252452e83a4441cb27
case547 <9521840+case547@users.noreply.github.com>
2021-07-20 16:19:04 +0100

    Create 2.1-exercise.py
    Wrote up code for insertion sort

---
+++ i-foundations/2-getting-started/2.1-exercise.py
@@ -0,0 +1,11 @@
+def insertion_sort(arr):
+    for j in range(1, len(arr)):  # starting with second index in array
+        key = arr[j]
+        i = j - 1
+
+        # Insert arr[j] into sorted sequence arr[0..j-1]
+        while i > 0 and arr[i] > key:
+            arr[i+1] = arr[i]   # copy arr[i] value one space to the right
+            i -= 1  # repeat while key < preceeding values
+
+        arr[i+1] = key  # place key in position
