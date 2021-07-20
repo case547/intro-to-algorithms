d6bde3a87e84aea8587a932b42c24634fed93a32
case547 <9521840+case547@users.noreply.github.com>
2021-07-20 16:41:56 +0100

    insertion sort subchapter

--- i-foundations/2-getting-started/insertion_sort.py
+++ i-foundations/2-getting-started/insertion_sort.py
@@ -4,8 +4,10 @@ def insertion_sort(arr):
         i = j - 1
 
         # Insert arr[j] into sorted sequence arr[0..j-1]
-        while i > 0 and arr[i] > key:
+        while i >= 0 and arr[i] > key:
             arr[i+1] = arr[i]   # copy arr[i] value one space to the right
             i -= 1  # repeat while key < preceeding values
 
         arr[i+1] = key  # place key in position
+
+    return arr
